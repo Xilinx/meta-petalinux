67e1166dc568ce381be5b49b0e359422022dfd58
Manjukumar Matha <manjukumar.harthikote-matha@xilinx.com>
2018-04-27 14:45:07 -0700

    ultra96-ap-setup_1.0.bb: Add recipe to create AP for Ultra96
    Add recipe to create AP for Ultra96. This adds scripts to read the MAC
    address from the board and split the interface to create wlan1 in
    managed mode for AP
    
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>
    Signed-off-by: Manjukumar Matha <manjukumar.harthikote-matha@xilinx.com>

---
+++ recipes-utils/ultra96-ap-setup/files/ap.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#*******************************************************************************
+#
+# Copyright (C) 2016 - 2017 Xilinx, Inc. All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+#
+# Use of the Software is limited solely to applications:
+#
+# (a) running on a Xilinx device, or
+# (b) that interact with a Xilinx device through a bus or interconnect.
+#
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+# OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+# Except as contained in this notice, the name of the Xilinx shall not be used
+# in advertising or otherwise to promote the sale, use or other dealings in
+# this Software without prior written authorization from Xilinx.
+#
+# Author:     Bhargava Sreekantappa Gayathri <bsreekan@xilinx.com>
+#
+# ******************************************************************************
+
+cd ${datadir}/wpa_ap
+
+for index in {1..10}:
+do
+	wlan0_status=$(wpa_cli -i wlan0 ping)
+	if [ $wlan0_status == "PONG" ]; then
+		wlan0_found=true
+		break
+	fi
+	sleep 1
+done
+
+
+#Create a new managed mode interface wlan1 to run AP
+iw phy phy0 interface add wlan1 type managed
+
+hid=$(ifconfig -a | grep wlan1 | sed "s,wlan1.*HWaddr \(.*\),\1," | tr -d ": ")
+ip=192.168.2.1
+
+sed "s,Ultra96,Ultra96_$hid," wpa_ap.conf > wpa_ap_actual.conf
+
+ifdown wlan1
+sleep 2
+wpa_supplicant -c ./wpa_ap_actual.conf  -iwlan1 &
+
+ifconfig wlan1 $ip
+touch /var/lib/misc/udhcpd.leases
+udhcpd ./udhcpd.conf
---
+++ recipes-utils/ultra96-ap-setup/ultra96-ap-setup_1.0.bb
@@ -0,0 +1,37 @@
+DESCRIPTION = "Creates access point(AP) on ultra96"
+SUMMARY = "Reads MAC address from board and splits the interface to create AP"
+
+LICENSE = "Proprietary"
+LIC_FILES_CHKSUM = "file://ap.sh;beginline=2;endline=37;md5=b3a1c64c07b72476ff1ae6dc4b5574c8"
+
+SRC_URI = "file://ap.sh \
+	file://udhcpd.conf \
+	file://wpa_ap.conf \
+	file://ultra96-ap-setup.sh \
+	"
+
+FILES_${PN} += "${datadir}/wpa_ap"
+
+inherit update-rc.d
+
+INITSCRIPT_NAME = "ultra96-ap-setup.sh"
+INITSCRIPT_PARAMS = "start 99 S ."
+
+COMPATIBLE_MACHINE = "^$"
+COMPATIBLE_MACHINE_ultra96-zynqmp = "ultra96-zynqmp"
+
+RDEPENDS_${PN} = "wpa-supplicant busybox"
+
+S = "${WORKDIR}"
+
+do_install () {
+    install -d ${D}${datadir}/wpa_ap
+    install -d ${D}${sysconfdir}/init.d/
+
+    install -m 0755 ${WORKDIR}/ultra96-ap-setup.sh ${D}${sysconfdir}/init.d/ultra96-ap-setup.sh
+    install -m 0644 ${WORKDIR}/wpa_ap.conf  ${D}${datadir}/wpa_ap/wpa_ap.conf
+    install -m 0644 ${WORKDIR}/udhcpd.conf  ${D}${datadir}/wpa_ap/udhcpd.conf
+    install -m 0755 ${WORKDIR}/ap.sh	${D}${datadir}/wpa_ap/ap.sh
+
+}
+
