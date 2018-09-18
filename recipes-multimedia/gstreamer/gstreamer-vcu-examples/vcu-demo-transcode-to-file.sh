db8e9a38040411f127f7fa9713c0906fa4f7039c
Devarsh Thakkar <devarsht@xilinx.com>
2018-09-17 18:37:42 -0700

    vcu-demo-transcode-to-file.sh: Add support to transcode aac and vorbis audio
    - Add support to transcode audio from aac to vorbis and vice-versa
      if specified by user using -a option.
    
    - Add commandline option -a/--audio-type using which user can specify what
      type of audio is present in given input container.
    
    - Based upon the input file script and user provided codec type (-c) and audio
      input type (-a), script will do below conversions (transcoding) :
       - h264->h265 video and vice-versa
       - aac->vorbis audio and vice-versa
       - mkv->mp4 and vice-versa
    
    - If audio input type is not specified, then script will just do the video transcoding.
    
    Usage: vcu-demo-transcode-to-file.sh -i /mnt/sata/2160p_30.mp4 -c avc -a aac
    
    Signed-off-by: Devarsh Thakkar <devarsht@xilinx.com>
    Acked-by: Varunkumar Allagadapa <varunkumar.allagadapa@xilinx.com>
    Signed-off-by: Jaewon Lee <jaewon.lee@xilinx.com>
    Signed-off-by: Manjukumar Matha <manjukumar.harthikote-matha@xilinx.com>

--- recipes-multimedia/gstreamer/gstreamer-vcu-examples/vcu-demo-transcode-to-file.sh
+++ recipes-multimedia/gstreamer/gstreamer-vcu-examples/vcu-demo-transcode-to-file.sh
@@ -30,7 +30,7 @@ fi
 source vcu-demo-functions.sh
 
 scriptName=`basename $0`
-declare -a scriptArgs=("inputPath" "codecType" "showFps" "targetBitrate" "outputPath" "internalEntropyBuffers")
+declare -a scriptArgs=("inputPath" "codecType" "showFps" "targetBitrate" "outputPath" "internalEntropyBuffers" "audioType")
 declare -a checkEmpty=("inputPath" "targetBitrate")
 
 
@@ -40,16 +40,16 @@ declare -a checkEmpty=("inputPath" "targetBitrate")
 # Description:	To display script's command line argument help
 ############################################################################
 usage () {
-	echo '	Usage : '$scriptName' -i <input_file_path> -b <--bit-rate> -c <codec_type> -o <output_path> -e <internal_entropy_buffers> -f'
+	echo '	Usage : '$scriptName' -i <input_file_path> -b <--bit-rate> -c <codec_type> -a <audio_type> -o <output_path> -e <internal_entropy_buffers> -f'
 	DisplayUsage "${scriptArgs[@]}"
 	echo '  Example :'
 	echo '  '$scriptName' -i /run/2160p_30.h264 -b 5000'
 	echo '  '$scriptName' -i /run/2160p_30.h264 -b 5000 -f'
 	echo '  '$scriptName' -i /run/2160p_60.h264 -o /mnt/sata/op.h265'
 	echo '  '$scriptName' -i /run/2160p_60.h264 -f'
-	echo '  '$scriptName' -i /mnt/sata/2160p_30.mp4 -c avc'
+	echo '  '$scriptName' -i /mnt/sata/2160p_30.mp4 -c avc -a aac'
 	echo '  '$scriptName' -i /mnt/sata/2160p_60.mp4 -c avc -e 7'
-	echo '  '$scriptName' -i /mnt/sdcard/2160p_30.mkv -c hevc'
+	echo '  '$scriptName' -i /mnt/sdcard/2160p_30.mkv -c hevc -a vorbis'
 	echo '  "NOTE: This script depends on vcu-demo-functions.sh to be present in /usr/bin or its path set in $PATH"'
 	exit
 }
@@ -60,69 +60,115 @@ usage () {
 # Description:	Decode input encoded file to YUV
 ############################################################################
 TranscodeFile() {
-	FILE_NAME=$(basename "$INPUT_PATH")
-	EXT_TYPE="${FILE_NAME##*.}"
+	INPUT_FILE_NAME=$(basename "$INPUT_PATH")
+	INPUT_EXT_TYPE="${INPUT_FILE_NAME##*.}"
 
-	if [ $EXT_TYPE == "mp4" -o $EXT_TYPE == "mkv" ] && [ -z $CODEC_TYPE ]; then
-		echo "No codec type specified for $FILE_NAME hence assuming avc as default codec"
+	if [ $INPUT_EXT_TYPE == "mp4" -o $INPUT_EXT_TYPE == "mkv" ] && [ -z $CODEC_TYPE ]; then
+		echo "No codec type specified for $INPUT_FILE_NAME hence assuming avc as default codec"
 		CODEC_TYPE="avc"
 	fi
 
+        case $AUDIODEC_TYPE in
+        "aac")
+		AUDIODEC="faad"
+		AUDIOPARSE="vorbisparse"
+		AUDIOENC="vorbisenc";;
+        "vorbis")
+		AUDIODEC="vorbisdec"
+		AUDIOPARSE="aacparse"
+		AUDIOENC="faac";;
+         *)
+                if ! [ -z $AUDIODEC_TYPE ]; then
+                        ErrorMsg "Invalid audio codec type specified, please specify either vorbis or aac"
+                fi
+        esac
+
 	if [ -z $OUTPUT_PATH ]; then
 		DIR_NAME=$(pwd)
-		FILE_NAME_WITHOUTEXT="${FILE_NAME%.*}"
-		if [ $EXT_TYPE == "mp4" -o $EXT_TYPE == "mkv" ] && [ $CODEC_TYPE == "avc" ]; then
+		FILE_NAME_WITHOUTEXT="${INPUT_FILE_NAME%.*}"
+		if [ $INPUT_EXT_TYPE == "mp4" ]; then
+			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".mkv""
+		elif [ $INPUT_EXT_TYPE == "mkv" ]; then
+			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".mp4""
+		elif [ $INPUT_EXT_TYPE == "h264" ]; then
 			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".h265""
-		elif [ $EXT_TYPE == "mp4" -o $EXT_TYPE == "mkv" ] && [ $CODEC_TYPE == "hevc" ]; then
+		elif [ $INPUT_EXT_TYPE == "h265" ]; then
 			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".h264""
-		elif [ $EXT_TYPE == "h264" -o $EXT_TYPE == "avc" ]; then
-			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".h265""
+		elif [ $INPUT_EXT_TYPE == "avc" ]; then
+			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".hevc""
 		else
-			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".h264""
+			OUTPUT_PATH="$DIR_NAME/$FILE_NAME_WITHOUTEXT".aac""
 		fi
-
 		echo "No output path specified hence using $OUTPUT_PATH as default"
 	fi
 
-	OUT_FILE_NAME=$(basename "$OUTPUT_PATH")
-	OUT_EXT_TYPE="${OUT_FILE_NAME##*.}"
+	OUTPUT_FILE_NAME=$(basename "$OUTPUT_PATH")
+	OUTPUT_EXT_TYPE="${OUTPUT_FILE_NAME##*.}"
+
+	case $OUTPUT_EXT_TYPE in
+        "h264" | "avc")
+                CODEC_TYPE="hevc";;
+        "h265" | "hevc")
+                CODEC_TYPE="avc";;
+	"mp4")
+		MUX="qtmux name=mux";;
+	"mkv")
+		MUX="matroskamux name=mux";;
+	esac
+
+	case $INPUT_EXT_TYPE in
+	"mp4")
+		DMUX=$QTDEMUX;;
+	"mkv")
+		DMUX=$MTDEMUX;;
+	esac
 
 	FILE_SRC="$FILE_SRC=$INPUT_PATH"
 	QUEUE="$QUEUE max-size-bytes=0"
 	SINK="$FILESINK=$OUTPUT_PATH"
 
 	if [ $SHOW_FPS ]; then
-		SINK="fpsdisplaysink name=fpssink text-overlay=false video-sink=fakevideosink sync=true -v"
+		SINK="fpsdisplaysink name=fpssink text-overlay=false video-sink=fakesink sync=true -v"
 	fi
 
 	OMXH264ENC="$OMXH264ENC control-rate=2 b-frames=2 gop-length=999 prefetch-buffer=true target-bitrate=$BIT_RATE ! video/x-h264, profile=high"
 	OMXH265ENC="$OMXH265ENC control-rate=2 b-frames=2 gop-length=999 prefetch-buffer=true target-bitrate=$BIT_RATE ! video/x-h265, profile=main,level=\(string\)6.2,tier=main"
 
-	if [ $EXT_TYPE == "h264" -o $EXT_TYPE == "avc" ]; then
-		pipeline="$GST_LAUNCH $FILE_SRC ! $H264PARSE ! $OMXH264DEC ! $QUEUE ! $OMXH265ENC ! $SINK"
-	elif [ $EXT_TYPE == "h265" -o $EXT_TYPE == "hevc" ];then
-		pipeline="$GST_LAUNCH $FILE_SRC ! $H265PARSE ! $OMXH265DEC ! $QUEUE ! $OMXH264ENC ! $SINK"
-	elif [ $EXT_TYPE == "mp4" ]; then
-		if [ $CODEC_TYPE == "avc" ]; then
-			pipeline="$GST_LAUNCH $FILE_SRC ! $QTDEMUX ! $H264PARSE ! $OMXH264DEC ! $QUEUE ! $OMXH265ENC ! $SINK"
-		else
-			pipeline="$GST_LAUNCH $FILE_SRC ! $QTDEMUX ! $H265PARSE ! $OMXH265DEC ! $QUEUE ! $OMXH264ENC ! $SINK"
-		fi
-	elif [ $EXT_TYPE == "mkv" ]; then
-		if [ $CODEC_TYPE == "avc" ]; then
-			pipeline="$GST_LAUNCH $FILE_SRC ! $MTDEMUX ! $H264PARSE ! $OMXH264DEC ! $QUEUE ! $OMXH265ENC ! $SINK"
-		else
-			pipeline="$GST_LAUNCH $FILE_SRC ! $MTDEMUX ! $H265PARSE ! $OMXH265DEC ! $QUEUE ! $OMXH264ENC ! $SINK"
-		fi
+	case $CODEC_TYPE in
+	"avc")
+		DEC_PARSER=$H264PARSE
+		DECODER=$OMXH264DEC
+		ENC_PARSER=$H265PARSE
+		ENCODER=$OMXH265ENC;;
+	"hevc")
+		DEC_PARSER=$H265PARSE
+		DECODER=$OMXH265DEC
+		ENC_PARSER=$H264PARSE
+		ENCODER=$OMXH264ENC;;
+	esac
+
+	if [[ $DMUX && $MUX && $AUDIODEC_TYPE ]]; then #e.g .mp4 to .mkv with audio transcode
+		pipeline="$GST_LAUNCH $FILE_SRC ! $DMUX ! $MULTIQUEUE name=mq max-size-time=0 ! $DEC_PARSER ! $DECODER ! $QUEUE max-size-bytes=0 ! $ENCODER ! $ENC_PARSER ! $QUEUE name=vq ! $MUX ! $SINK demux.audio_0 ! mq.sink_1 mq.src_1 ! $AUDIODEC ! $AUDIOCONVERT ! $AUDIORESAMPLE ! $AUDIOENC ! $AUDIOPARSE ! $QUEUE name=aq ! mux.audio_0"
+	elif [[ $DMUX && $MUX && ! $AUDIODEC_TYPE ]]; then #e.g .mp4 to .mkv without audio transcode
+		pipeline="$GST_LAUNCH $FILE_SRC ! $DMUX ! $DEC_PARSER ! $DECODER ! $QUEUE ! $ENCODER ! $ENC_PARSER ! $MUX ! $SINK"
+	elif [[ $DMUX && ! $MUX ]]; then #e.g .mp4 to .h264 transcode
+		pipeline="$GST_LAUNCH $FILE_SRC ! $DMUX ! $DEC_PARSER ! $DECODER ! $QUEUE ! $ENCODER ! $SINK"
+        elif [[ ! $DMUX && $MUX ]]; then #e.g .h264 to .mp4 transcode
+                pipeline="$GST_LAUNCH $FILE_SRC ! $DEC_PARSER ! $DECODER ! $QUEUE ! $ENCODER ! $ENC_PARSER ! $MUX ! $SINK"
+	elif [[ ! $DMUX && ! $MUX ]]; then #e.g .h264 to .h265 transcode
+		pipeline="$GST_LAUNCH $FILE_SRC ! $DEC_PARSER ! $DECODER ! $QUEUE ! $ENCODER ! $SINK"
 	else
-		ErrorMsg "Incorrect Input file path provided"
+		ErrorMsg "Incorrect output file provided"
 	fi
 
 	runGstPipeline "$pipeline"
+	if [ $? -ne 0 ]; then
+		ErrorMsg "Incorrect input/output file format"
+	fi
 }
 
 # Command Line Argument Parsing
-args=$(getopt -o "i:c:b:o:e:fh" --long "input-path:,codec-type:,bit-rate:,output-path:,internal-entropy-buffers:,show-fps,help" -- "$@")
+args=$(getopt -o "i:c:b:a:o:e:fh" --long "input-path:,codec-type:,bit-rate:,audio-type:,output-path:,internal-entropy-buffers:,show-fps,help" -- "$@")
 
 [ $? -ne 0 ] && usage && exit -1
 
