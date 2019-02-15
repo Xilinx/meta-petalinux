5ccf7181b155d3f074795361cf686b471f708bda
Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>
2019-02-14 17:08:33 -0800

    devtool: provide support for devtool configure command
    devtool configure command allows users to configure the source by
    enabling and disabling any build time and configuration options for
    packages being built. devtool configure is useful for specific packages
    like device-tree, fsbl, pmu-firmware when xsct yaml flags are modified.
    In such scenarios configure option will call xsct with the modified
    arguments. By default, this task will run oe_runmake clean if a makefile
    is found and CLEANBROKEN is not set to 1. If no such file is found or
    CLEANBROKEN is set to 1 then do_configure does nothing.
    
    syntax:
         devtool configure <package-name>
    
    For example: devtool configure device-tree this will call xsct <hdf>
    {APP_ARG} {MISC_ARG}
    
    The artifacts from the configure step will be present in the bitbake
    workdir.
    
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>
    Signed-off-by: Alejandro Enedino Hernandez Samaniego <alejandr@xilinx.com>
    Signed-off-by: Manjukumar Matha <manjukumar.harthikote-matha@xilinx.com>

---
+++ lib/devtool/configure.py
@@ -0,0 +1,72 @@
+#*******************************************************************************
+#
+# Copyright (C) 2018 Xilinx, Inc. All rights reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+# OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+# Except as contained in this notice, the name of the Xilinx shall not be used
+# in advertising or otherwise to promote the sale, use or other dealings in
+# this Software without prior written authorization from Xilinx.
+#
+# Author: chandana kalluri <ckalluri@xilinx.com>
+#
+# ******************************************************************************
+
+
+"""Devtool configure plugin"""
+
+import os
+import bb
+import logging
+import argparse
+import re
+import glob
+from devtool import setup_tinfoil, parse_recipe, DevtoolError, standard, exec_build_env_command
+
+logger = logging.getLogger('devtool')
+
+def configure(args, config, basepath, workspace):
+    """Entry point for the devtool 'configure' subcommand"""
+
+    if args.component not in workspace:
+           raise DevtoolError("recipe %s is not in your workspace, run devtool modify command first" % args.component)
+
+    rd = "" 
+    tinfoil = setup_tinfoil(basepath=basepath)
+    try:
+      rd = parse_recipe(config, tinfoil, args.component, appends=True, filter_workspace=False)
+      if not rd:
+         return 1
+
+      pn =  rd.getVar('PN', True)
+      if pn not in workspace:
+         raise DevtoolError("Run devtool modify before calling configure for %s" %pn)
+
+    finally:
+      tinfoil.shutdown()
+
+    exec_build_env_command(config.init_path, basepath, 'bitbake -c configure %s' % pn, watch=True) 
+
+    return 0
+
+def register_commands(subparsers, context):
+    """register devtool subcommands from this plugin"""
+    parser_configure = subparsers.add_parser('configure',help='runs configure command', description='runs configure for a specific package', group='advanced') 
+    parser_configure.add_argument('component', help='compenent to alter config')
+    parser_configure.set_defaults(func=configure,fixed_setup=context.fixed_setup)
