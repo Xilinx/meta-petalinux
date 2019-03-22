94ead0e176c2ce56939b080a1e61ec6e6c756f08
Manjukumar Matha <manjukumar.harthikote-matha@xilinx.com>
2019-03-21 22:46:43 -0700

    Revert "Add support for chromium-x11 in petalinux"
    This reverts commit 126e82683206448eb3d01b6d92532faade914dde.
    Newer version of Chromium does not support RHEL 7.x
    
    Signed-off-by: Manjukumar Matha <manjukumar.harthikote-matha@xilinx.com>

--- recipes-core/packagegroups/packagegroup-petalinux-ultra96-webapp.bb
+++ recipes-core/packagegroups/packagegroup-petalinux-ultra96-webapp.bb
@@ -3,7 +3,7 @@ DESCRIPTION = "Required packages for ultra96 startup pages"
 inherit packagegroup
 
 ULTRA96_STARTUP_PAGES_PACKAGES = " \
-	chromium-x11 \
+	chromium \
 	ace-cloud-editor \
 	python3-flask \
 	python3-werkzeug \
--- recipes-utils/ultra96-startup-pages/files/launch-ultra96-startup-page.sh
+++ recipes-utils/ultra96-startup-pages/files/launch-ultra96-startup-page.sh
@@ -3,7 +3,7 @@
 source /usr/share/ultra96-startup-pages/webapp/static/ultra96-startup-page.conf
 if [ $webapp_on_boot = 1 ]; then
 	echo -e "Opening webpage"
-	chromium --no-sandbox  --use-egl --user-data-dir  "http://localhost:80"
+	chromium --use-egl --user-data-dir  "http://localhost:80"
 else
 	echo -e "Not opening webpage"
 fi
