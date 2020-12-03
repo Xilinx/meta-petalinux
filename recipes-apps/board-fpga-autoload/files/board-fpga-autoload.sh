98941cd5c48b68a128ffbe7e595cfebc5035c919
Raju Kumar Pothuraju <raju.kumar-pothuraju@xilinx.com>
2020-12-03 09:21:25 +0530

    board-fpga-autoload.sh: Update dtbo,bin path
    Updating dtbo,bin files path to /lib/firmware/xilinx.
    
    Signed-off-by: Raju Kumar Pothuraju <raju.kumar-pothuraju@xilinx.com>
    Signed-off-by: Jaewon Lee <jaewon.lee@xilinx.com>

--- recipes-apps/board-fpga-autoload/files/board-fpga-autoload.sh
+++ recipes-apps/board-fpga-autoload/files/board-fpga-autoload.sh
@@ -6,14 +6,14 @@ if [ ! -z $dev_eeprom ] && [ -f $dev_eeprom ]; then
 	board_rev=$(dd if=$dev_eeprom bs=1 count=3 skip=224 2>/dev/null)
 
 	echo board_name:$board_name - board_rev:$board_rev
-	BD_DTPATH="/lib/firmware/${board_name}_${board_rev}"
+	BD_DTPATH="/lib/firmware/xilinx/${board_name}_${board_rev}"
 	if [ -d ${BD_DTPATH} ];then
 		BIN="$(find ${BD_DTPATH}/ -name *.bin | head -1)"
 		DTBO="$(find ${BD_DTPATH}/ -name *.dtbo | head -1)"
 	else
 		echo -e "\033[1mValid Board Information Not Found, Loading rev1.0 bitstream and dtbo as default (see fpgautil -h for removing and loading different bitstream and dtbo)\033[0m"
 		board_rev=1.0  # Loading Rev1.0 bitstream and dtbo in default case.
-		BD_DTPATH="/lib/firmware/*_${board_rev}"
+		BD_DTPATH="/lib/firmware/xilinx/*_${board_rev}"
 		BIN="$(find ${BD_DTPATH}/ -name *.bin | head -1)"
 		DTBO="$(find ${BD_DTPATH}/ -name *.dtbo | head -1)"
 	fi
