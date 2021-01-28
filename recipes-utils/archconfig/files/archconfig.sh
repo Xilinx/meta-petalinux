1af3c8589a957c75cbf2ee686d90f5880e202856
Jaewon Lee <jaewon.lee@xilinx.com>
2021-01-27 16:12:08 -0800

    archconfig.sh: simplifying BOARD_VARIANT
    Rather than doing a custom parse to form the BOARD_VARIANT value from
    SOM and CC values, just inserting a '_' ('-' is replaced with '_' for
    dnf config files in buildsystem)
    using the example of SOM = k26 and CC = kv
    BOARD_VARIANT would be k26_kv
    
    Signed-off-by: Jaewon Lee <jaewon.lee@xilinx.com>

--- recipes-utils/archconfig/files/archconfig.sh
+++ recipes-utils/archconfig/files/archconfig.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-somnum=`echo $(fru-print.py -b som -f product) | sed 's/.*-K\([0-9]*\).*/\1/'`
 som=`echo $(fru-print.py -b som -f product) | sed 's/.*-\(K[0-9]*\).*/\1/' | tr '[:upper:]' '[:lower:]'`
 cc=`echo $(fru-print.py -b cc -f product) | awk -F- '{ print $2}' | tr '[:upper:]' '[:lower:]'`
 
 #TODO Possibly check BOARD/BOARD_VARIANT values before changing arch file
 #(but will only affect if the values match up to a repo arch value)
 BOARD=${som}
-BOARD_VARIANT=${cc}${somnum}0
+BOARD_VARIANT=${som}_${cc}
 
 #check if dnf configs already updated based off BOARD value
 if ! grep "${BOARD}" /etc/dnf/vars/arch > /dev/null
