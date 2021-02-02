1a99a3aee0f6aadbffa1e3f9bf28524802164118
Jaewon Lee <jaewon.lee@xilinx.com>
2021-02-01 16:10:48 -0800

    xmutil.py: Adding getpkgs command in xmutil
    getpkgs will use fru-print to get the SOM+CC combination and parse
    the output of 'dnf list --available' to get a list of all packages
    compatible with the running board
    
    Signed-off-by: Jaewon Lee <jaewon.lee@xilinx.com>

--- recipes-utils/xmutil/files/xmutil.py
+++ recipes-utils/xmutil/files/xmutil.py
@@ -5,7 +5,6 @@ import subprocess, argparse
 cmds = { 'boardid':'/usr/bin/fru-print.py', \
         'bootfw':'', \
 	'bootfwupd':'', \
-	'getpkgs':'', \
 	'listapps':'dfx-mgr-client', \
 	'loadapp':'dfx-mgr-client', \
 	'unloadapp':'dfx-mgr-client', \
@@ -27,7 +26,13 @@ def bootfwupd(args):
     subprocess.run([cmds['bootfwupd']]+args)
 
 def getpkgs(args):
-    subprocess.run([cmds['getpkgs']]+args)
+    if ('-h' in args) or ('--help' in args):
+        print("\ngetpkgs will use fru-print to determine the SOM + CC combination\nand search the package feed for compatible packages\n")
+        exit()
+    som=subprocess.getoutput("fru-print.py -b som -f product | awk -F- '{ print $2}' | tr '[:upper:]' '[:lower:]'")
+    cc=subprocess.getoutput("fru-print.py -b cc -f product | awk -F- '{ print $2}' | tr '[:upper:]' '[:lower:]'")
+    print("\nSearching package feed for packages compatible with: " + som+'_'+cc+'\n')
+    subprocess.run('dnf list --available | grep ' + '\.' +som +'_'+ cc,shell=True)
 
 def listapps(args):
     subprocess.run([cmds['listapps'],'-listPackage'])
