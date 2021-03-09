c85e1078162c231a4e0494d4e5f77ad91189f118
Christian Kohn <christian.kohn@xilinx.com>
2021-03-09 11:11:22 -0800

    start-jupyerlab: Add recipe for jupyerlab-server daemon
    This recipe adds a daemon for the jupyterlab-server along with configuration
    files for jupyterlab.
    
    Signed-off-by: Christian Kohn <christian.kohn@xilinx.com>
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>

---
+++ recipes-utils/start-jupyterlab/start-jupyterlab/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.notebook_dir = '/opt/xilinx/share/notebooks'
+c.NotebookApp.port = 8888
+c.NotebookApp.iopub_data_rate_limit = 100000000
+c.NotebookApp.allow_root=True
+c.NotebookApp.open_browser=False
+
